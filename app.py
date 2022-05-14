5671b773e215fb2e9f6358c807e7198b8352201e
Natalia Kuzmina <kuzmina.nd@phystech.edu>
2022-05-14 11:11:37 +0300

    add selection from database

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+
+DB_PATH = '/tmp/test.s3db'
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return 'Hello'
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM t1')
+    return render_template(
+        'main_page.html',
+        columns=[x[0] for x in cur.description],
+        data=cur.fetchall()
+     )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5010, debug=True)
