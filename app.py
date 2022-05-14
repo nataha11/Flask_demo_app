e64cddc7b8e3189d8e5e65d0e49e6acd29b962ff
Natalia Kuzmina <kuzmina.nd@phystech.edu>
2022-05-14 11:51:44 +0300

    Implemented a form for adding entries into db

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 import sqlite3
 
 DB_PATH = '/tmp/test.s3db'
 
 app = Flask(__name__)
 
+@app.route('/add', methods=['POST'])
+def add_item():
+    # Fetch and validate input data
+    a = request.form.get('a')
+    b = request.form.get('b')
+    if a is None or b is None:
+        abort(400)
+    try:
+        a = int(a)
+    except ValueError:
+        abort(400)
+    # Try to insert a new entry:
+    with sqlite3.conect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute('INSERT INTO t1(a, b) VALUES (?, ?)', (a, b))
+        conn.commit()
+        # Tell user to load main page again
+    return redirect(url_for('main_page'))  
+      
 @app.route('/')
 def main_page():
     with sqlite3.connect(DB_PATH) as conn:
